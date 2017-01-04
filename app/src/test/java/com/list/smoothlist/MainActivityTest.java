6a64afde88d0e09eff3c2873b83b7cc62c3e9390
Maxime liege <liege30@gmail.com>
2017-01-04 19:04:06 +0100

    Unit test + modif Travis
    Simple Unit test + travis modified

--- app/src/test/java/com/list/smoothlist/MainActivityTest.java
+++ app/src/test/java/com/list/smoothlist/MainActivityTest.java
@@ -2,7 +2,7 @@ package com.list.smoothlist;
 
 import junit.framework.TestCase;
 
-public class ToDoListTest extends TestCase {
+public class MainActivityTest extends TestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
